9c2ee9c5893ec46d7a16584e80b86521a362690a
Shashaank <srkota1951@gmail.com>
2022-03-18 18:07:24 +0530

    added the Tv_List and Devices_List activities

--- app/src/main/java/com/practice/remoteapp/Devices_List.java
+++ app/src/main/java/com/practice/remoteapp/Devices_List.java
@@ -6,40 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.widget.ImageButton;
 
 public class Devices_List extends AppCompatActivity {
 
-    ListView devicesListView;
-    ArrayList<String> deviceName = new ArrayList<String>();
+    ImageButton tvImageButton, acImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devices_list);
 
-        devicesListView = findViewById(R.id.devices_List_View);
-        deviceName.add("Samsung Tv");
-        deviceName.add("LG Tv");
+        tvImageButton = findViewById(R.id.tvImageButton);
+        acImageButton = findViewById(R.id.tvImageButton);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, deviceName);
-        devicesListView.setAdapter(arrayAdapter);
 
-        devicesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        tvImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra("devicePosition", position);
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Tv_List.class);
                 startActivity(intent);
-                Log.d("Position", "onItemClick: " + position);
+                Log.d("Intent", "Tv activity started");
             }
         });
-
     }
+
 }
